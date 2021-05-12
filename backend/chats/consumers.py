d577e7846a3aa53fe6bc6d8e33fc5e064ccd0f07
Voldemat <77781574+Voldemat@users.noreply.github.com>
2021-05-13 00:36:16 +0300

    'close_websocket_if_user_not_authenticated'

--- backend/chats/consumers.py
+++ backend/chats/consumers.py
@@ -1,15 +1,24 @@
 import json
 from channels.generic.websocket import AsyncWebsocketConsumer
+from channels.exceptions import StopConsumer
 
 class ChatConsumer(AsyncWebsocketConsumer):
-    async def connect(self):
-        dict_keys = self.scope.keys()
-        for item in dict_keys:
-            print(f'{item}:{self.scope[item]}')
 
+    def _get_room_group_name(self, room_name):
+        return f'chat_{room_name}'
+    async def connect(self):
         url_kwargs = self.scope['url_route']['kwargs']
         self.room_name = url_kwargs['room_name']
-        self.room_group_name = f'chat_{self.room_name}'
+
+        user = self.scope['user']
+        # if not user close websocket
+        if user.is_anonymous:
+            print('websocket close')
+            await super().close(close_code = json.dumps({
+                "message":"User must be authenticated"}))
+
+        print(user)
+        self.room_group_name = self._get_room_group_name(self.room_name)
 
         print('___________')
 
@@ -21,12 +30,15 @@ class ChatConsumer(AsyncWebsocketConsumer):
         )
         await self.accept()
 
+        
+
     async def disconnect(self, close_code):
         # Leave room group
         await self.channel_layer.group_discard(
             self.room_group_name,
             self.channel_name
         )
+        await super().close(close_code)
 
     async def receive(self, text_data):
         text_data_json = json.loads(text_data)
